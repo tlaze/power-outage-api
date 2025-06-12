2d375adb2dd92ef7ff76b7e962941696867fdbf7
Tom <tomlazore@gmail.com>
2025-06-12 19:33:46 -0400

    refactored to use postgres in Render

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OutagesModule } from './outages/outages.module';
 
 @Module({
-  imports: [
+  //LOCAL HOST CONNECTION
+  // imports: [
+  //   ConfigModule.forRoot({ isGlobal: true }),
+  //   TypeOrmModule.forRoot({
+  //     type: 'postgres',
+  //     host: process.env.DB_HOST,
+  //     port: parseInt(process.env.DB_PORT),
+  //     username: process.env.DB_USERNAME,
+  //     password: process.env.DB_PASSWORD,
+  //     database: process.env.DB_NAME,
+  //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  //     synchronize: true,
+  //   }),
+  //   OutagesModule,
+  // ],
+    imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
       synchronize: true,
     }),
     OutagesModule,
